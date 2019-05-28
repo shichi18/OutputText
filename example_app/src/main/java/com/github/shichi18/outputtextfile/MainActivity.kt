5c12a0f93528d6a0d51eeab71080a783f08e7d9e
shichi <n.shichi18@gmail.com>
2019-05-28 12:44:19 +0900

    [refactor] refactor file & code.

--- example_app/src/main/java/com/github/shichi18/outputtextfile/MainActivity.kt
+++ example_app/src/main/java/com/github/shichi18/outputtextfile/MainActivity.kt
@@ -3,9 +3,7 @@ package com.github.shichi18.outputtextfile
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.LinearLayout
-import android.widget.TextView
 import com.github.shichi18.textviewoutputlibrary.FileOutput
-import java.io.InputStream
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,26 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val fileOutput = FileOutput()
         val assetManager = resources.assets
-        val list = fileOutput.displayAssets("InputData", assetManager)
-
         val linearLayout = findViewById<LinearLayout>(R.id.linearlayout)
-        val match = LinearLayout.LayoutParams.MATCH_PARENT
-        val layoutParams = LinearLayout.LayoutParams(match, match)
-        layoutParams.setMargins(0, 20, 0, 20)
-
-        for (i in list!!.indices) {
-            val inputStream: InputStream = assetManager.open("InputData/" + list[i])
-            val inputString = fileOutput.fileInput(inputStream)
-            val textView = TextView(this)
-
-            textView.setBackgroundResource(R.drawable.border)
-            textView.setPadding(20, 20, 20, 20)
-            textView.text = inputString
-            textView.layoutParams = layoutParams
-            linearLayout.addView(textView)
-        }
+        fileOutput.displayAssets(this,"InputData",assetManager,linearLayout)
     }
 }
--- textviewoutputlibrary/src/main/java/com/github/shichi18/textviewoutputlibrary/FileOutput.kt
+++ textviewoutputlibrary/src/main/java/com/github/shichi18/textviewoutputlibrary/FileOutput.kt
@@ -1,6 +1,10 @@
 package com.github.shichi18.textviewoutputlibrary
 
+import android.app.Activity
+import android.content.Context
 import android.content.res.AssetManager
+import android.widget.LinearLayout
+import android.widget.TextView
 import java.io.IOException
 import java.io.InputStream
 
@@ -10,11 +14,30 @@ class FileOutput {
         return inputStream.bufferedReader().use { it.readText() }
     }
 
-    fun displayAssets(dir: String, assetManager: AssetManager): Array<String>? {
+    fun displayAssets(
+        context: Context,
+        dir: String,
+        assetManager: AssetManager,
+        linearLayout: LinearLayout
+    ): Array<String>? {
         val files: Array<String>? = assetManager.list(dir)
+        val match = LinearLayout.LayoutParams.MATCH_PARENT
+
+        val layoutParams = LinearLayout.LayoutParams(match, match)
+        layoutParams.setMargins(0, 20, 0, 20)
+
         try {
             for (i in files!!.indices) {
                 println(files[i])
+                val inputStream: InputStream = assetManager.open("InputData/" + files[i])
+                val inputString = fileInput(inputStream)
+                val textView = TextView(context)
+
+                textView.setBackgroundResource(R.drawable.border)
+                textView.setPadding(20, 20, 20, 20)
+                textView.text = inputString
+                textView.layoutParams = layoutParams
+                linearLayout.addView(textView)
 
             }
             return files
