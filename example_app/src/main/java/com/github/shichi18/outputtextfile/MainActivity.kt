ae68dff0d69704af838148c7f1b8c6e6784a6500
shichi <n.shichi18@gmail.com>
2019-05-28 13:04:09 +0900

    [refactor & add] refactor code & add icon.

--- example_app/src/main/java/com/github/shichi18/outputtextfile/MainActivity.kt
+++ example_app/src/main/java/com/github/shichi18/outputtextfile/MainActivity.kt
@@ -3,7 +3,7 @@ package com.github.shichi18.outputtextfile
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.LinearLayout
-import com.github.shichi18.textviewoutputlibrary.FileOutput
+import com.github.shichi18.textviewoutputlibrary.PushTextView
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val fileOutput = FileOutput()
+        val pushTextView = PushTextView()
         val assetManager = resources.assets
         val linearLayout = findViewById<LinearLayout>(R.id.linearlayout)
-        fileOutput.displayAssets(this,"InputData",assetManager,linearLayout)
+        pushTextView.textViewAssets(this,"InputData",assetManager,linearLayout)
     }
 }
--- textviewoutputlibrary/src/main/java/com/github/shichi18/textviewoutputlibrary/PushTextView.kt
+++ textviewoutputlibrary/src/main/java/com/github/shichi18/textviewoutputlibrary/PushTextView.kt
@@ -1,6 +1,5 @@
 package com.github.shichi18.textviewoutputlibrary
 
-import android.app.Activity
 import android.content.Context
 import android.content.res.AssetManager
 import android.widget.LinearLayout
@@ -8,18 +7,19 @@ import android.widget.TextView
 import java.io.IOException
 import java.io.InputStream
 
-class FileOutput {
+class PushTextView {
 
-    fun fileInput(inputStream: InputStream): String {
+    private fun fileInput(assetManager: AssetManager, files: String, dir: String): String {
+        val inputStream: InputStream = assetManager.open("$dir/$files")
         return inputStream.bufferedReader().use { it.readText() }
     }
 
-    fun displayAssets(
+    fun textViewAssets(
         context: Context,
         dir: String,
         assetManager: AssetManager,
         linearLayout: LinearLayout
-    ): Array<String>? {
+    ) {
         val files: Array<String>? = assetManager.list(dir)
         val match = LinearLayout.LayoutParams.MATCH_PARENT
 
@@ -28,22 +28,17 @@ class FileOutput {
 
         try {
             for (i in files!!.indices) {
-                println(files[i])
-                val inputStream: InputStream = assetManager.open("InputData/" + files[i])
-                val inputString = fileInput(inputStream)
                 val textView = TextView(context)
-
                 textView.setBackgroundResource(R.drawable.border)
                 textView.setPadding(20, 20, 20, 20)
-                textView.text = inputString
+                textView.text = fileInput(assetManager, files[i], dir)
                 textView.layoutParams = layoutParams
                 linearLayout.addView(textView)
-
             }
-            return files
+
         } catch (e: IOException) {
         }
-        return files
+
     }
 
 }
