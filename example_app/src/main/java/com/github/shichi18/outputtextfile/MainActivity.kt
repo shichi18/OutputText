8a6af36b91bc1fb9ba5153c85c71b65473c1acc6
shichi <n.shichi18@gmail.com>
2019-05-28 10:02:41 +0900

    [add] Add layout margin & layout design.

--- example_app/src/main/java/com/github/shichi18/outputtextfile/MainActivity.kt
+++ example_app/src/main/java/com/github/shichi18/outputtextfile/MainActivity.kt
@@ -19,11 +19,19 @@ class MainActivity : AppCompatActivity() {
         val list = fileOutput.displayAssets("InputData", assetManager)
 
         val linearLayout = findViewById<LinearLayout>(R.id.linearlayout)
+        val match = LinearLayout.LayoutParams.MATCH_PARENT
+        val layoutParams = LinearLayout.LayoutParams(match, match)
+        layoutParams.setMargins(0, 20, 0, 20)
+
         for (i in list!!.indices) {
             val inputStream: InputStream = assetManager.open("InputData/" + list[i])
             val inputString = fileOutput.fileInput(inputStream)
             val textView = TextView(this)
+
+            textView.setBackgroundResource(R.drawable.border)
+            textView.setPadding(20, 20, 20, 20)
             textView.text = inputString
+            textView.layoutParams = layoutParams
             linearLayout.addView(textView)
         }
     }
