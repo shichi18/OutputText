f14e329391a13734bc124fb12921fbe07cd4f385
shichi <n.shichi18@gmail.com>
2019-05-29 23:58:38 +0900

    [refactor] refactor rename.

---
+++ example_app/src/main/java/com/github/shichi18/outputtext/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.github.shichi18.outputtext
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.LinearLayout
+import com.github.shichi18.outputtextfile.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val outputTextView = OutputTextView()
+        val assetManager = resources.assets
+        val linearLayout = findViewById<LinearLayout>(R.id.linearlayout)
+        outputTextView.textViewAssets(this, "InputData", assetManager, linearLayout)
+    }
+}
